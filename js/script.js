6742bdd1b2ec72b45e99d854287fe3160d48bf06
qsrdev <matteo.albertini747@gmail.com>
2025-05-04 15:29:45 +0200

    feat: task done with no bonues

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 const arrNumbers = [];
 const timerElem = document.getElementById("timer");
 const randomNumElem = document.getElementById("random-numbers");
-const inputElem = document.getElementsByClassName("number-input");
-console.log(inputElem);
+const result = document.getElementById("result");
 
+//Inputs
+const inputElem1 = document.getElementById("numberInput1");
+const inputElem2 = document.getElementById("numberInput2");
+const inputElem3 = document.getElementById("numberInput3");
+const inputElem4 = document.getElementById("numberInput4");
+const inputElem5 = document.getElementById("numberInput5");
+
+//Bottoni per le funzioni
 const startBtn = document.getElementById("start-btn");
+const submitBtn = document.getElementById("submit-btn");
 
-let counter = 30;
+let counter = 5;
 
 startBtn.addEventListener("click", function () {
   setInterval(() => {
     if (counter === 0) {
       timerElem.innerHTML = "Tempo scaduto";
-      startBtn.innerHTML = "Conferma";
-      startBtn.disabled = false;
+      startBtn.classList.add("d-none");
+      submitBtn.classList.remove("d-none");
+      randomNumElem.innerHTML = `Ora prova a indovinare`;
     } else {
       counter--;
       timerElem.innerHTML = counter;
@@ -35,4 +44,28 @@ startBtn.addEventListener("click", function () {
   randomNumElem.innerHTML = `${arrNumbers[0]} - ${arrNumbers[1]} - ${arrNumbers[2]} - ${arrNumbers[3]} - ${arrNumbers[4]}`;
 });
 
+submitBtn.addEventListener("click", numbercheck);
+
 //Functions
+function numbercheck(event) {
+  event.preventDefault();
+
+  const firstNumber = inputElem1.value;
+  const secondNumber = inputElem2.value;
+  const thirdNumber = inputElem3.value;
+  const fourthNumber = inputElem4.value;
+  const fifthNumber = inputElem5.value;
+
+  const attInputNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber];
+  let correct = 0;
+  console.log(attInputNumbers);
+
+  for (let i = 0; i < arrNumbers.length; i++) {
+    if (attInputNumbers.includes(String(arrNumbers[i]))) {
+      correct++;
+    }
+  }
+
+  result.innerHTML = `Hai indovinato ${correct} numeri!`;
+  result.classList.remove("d-none");
+}
